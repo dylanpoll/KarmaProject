d520ddcbb036616268ca8971113cb3b86c9a290e
Dylan Poll <61894413+dylanpoll@users.noreply.github.com>
2020-05-25 15:57:21 -0400

    Add files via upload

--- codeKarmaProject/CodeKarmaApp/src/main/java/com/dylanpoll/CodeKarma/ReaderActivity.java
+++ codeKarmaProject/CodeKarmaApp/src/main/java/com/dylanpoll/CodeKarma/ReaderActivity.java
@@ -1,13 +1,19 @@
 package com.dylanpoll.CodeKarma;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -20,24 +26,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ReaderActivity extends AppCompatActivity {
-    private Button scan_btn;
 
+    private Button scan_btn;
+    EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reader);
+        text = (EditText) findViewById(R.id.port);
         scan_btn = (Button) findViewById(R.id.scan_btn);
         final Activity activity = this;
         scan_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 IntentIntegrator integrator = new IntentIntegrator(activity);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("Scan");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(true);
-                integrator.setBarcodeImageEnabled(false);
-                integrator.initiateScan();
+                    integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                    integrator.setPrompt("Scan");
+                    integrator.setCameraId(0);
+                    integrator.setBeepEnabled(true);
+                    integrator.setBarcodeImageEnabled(false);
+                    integrator.initiateScan();
             }
         });
     }
@@ -48,24 +56,21 @@ public class ReaderActivity extends AppCompatActivity {
             if(result.getContents()==null){Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();//failed to read
                 }
                 else {
-                    Toast.makeText(this, result.getContents(),Toast.LENGTH_LONG).show();//successful read shows text  Toast.makeText(object,object,object).show();
                 String karmaID = result.getContents();
-                postSignIn(karmaID);
+                String url = text.getText().toString().trim();
+                postSignIn(karmaID,url);
                 }
             }else{
             super.onActivityResult(requestCode, resultCode, data);}//failed to read so continue to read
     }
-    protected void postSignIn(String karmaID)  {
-        String url = "http://192.168.0.2:5000/users/signIn";
-
+    protected void postSignIn(String karmaID,String url)  {
         JSONObject body = new JSONObject();
             try {body.put("idtoken", karmaID);} catch (JSONException e) {e.printStackTrace();}//takes the QR code and packs it into the patch body.
-
         JsonObjectRequest request = new JsonObjectRequest
                 (Request.Method.PATCH, url, body, new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        //success
+                    public void onResponse(JSONObject response) {// this will echo back the users name + signed in.
+                        success(response);
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -78,4 +83,7 @@ public class ReaderActivity extends AppCompatActivity {
         MySingleton.getInstance(this).addToRequestQueue(request);
     }
 
+ public void success(JSONObject response){
+     Toast.makeText(this, response.toString(),Toast.LENGTH_LONG).show();//successful read shows text  Toast.makeText(object,object,object).show();
+ }
 }
